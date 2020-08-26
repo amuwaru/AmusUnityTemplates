1e470f3b0f53c83105c703f9eb323bd4a148c0a9
amuwaru <49254795+amuwaru@users.noreply.github.com>
2020-08-26 05:15:50 -0500

    Create FastMonoBehaviour.cs

---
+++ MonoBehaviours/FastMonoBehaviour.cs
@@ -0,0 +1,16 @@
+//made with amu's unity c# templates
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class #SCRIPTNAME# : MonoBehaviour
+{
+         void Start()
+         {
+
+         }
+         void Update()
+         {
+
+         }
+}
