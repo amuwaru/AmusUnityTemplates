842e928440103a8e636aa47765a40ad745e5ec95
amuwaru <49254795+amuwaru@users.noreply.github.com>
2020-08-26 05:12:10 -0500

    Create ExtendedMonoBehaviour.cs

---
+++ MonoBehaviours/ExtendedMonoBehaviour.cs
@@ -0,0 +1,48 @@
+//made with amu's unity c# templates
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+internal class #SCRIPTNAME# : MonoBehaviour
+{
+     #region FIELDS
+         //-public-\\
+         //-internal-\\
+         //-protected internal-\\
+         //-protected-\\
+         //-private protected-\\
+         //-private-\\
+     #endregion
+     #region CONSTRUCTORS & DESTRUCTORS
+         public void OnEnable() {} //This function is called when the behaviour is enabled.
+         public void OnDisable() {} //This function is called when the behaviour is disabled or the object is destroyed.
+     #endregion
+     #region DELEGATES, LAMBDAS, ANONS, EVENTS, INTERFACES, PROPERTIES, INDEXERS, ETC
+     #endregion
+     #region METHODS
+         //-magic-\\
+         public void Awake() {} //Awake is called when the script instance is being loaded.
+         public void Start() {} //Start is called on the frame when a script is enabled.
+         public void Update() {} //Update is called every frame.
+         public void FixedUpdate() {} //FixedUpdate is called multiple times per frame. Use for physics calculations.
+         public void LateUpdate() {}  //LateUpdate is called after all Update functions have been called.
+         //-public-\\
+         //-internal-\\
+         //-protected internal-\\
+         //-protected-\\
+         //-private-\\
+     #endregion
+     #region STRUCTS & CLASSES
+         //-public-\\
+         //-internal-\\
+         //-private-\\
+       
+         //-public-\\
+         //-protected internal-\\
+         //-protected-\\
+         //-internal-\\
+         //-private protected-\\
+         //-private-\\
+     #endregion
+}
