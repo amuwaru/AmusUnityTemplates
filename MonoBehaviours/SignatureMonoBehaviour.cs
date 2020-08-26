78b74c8d58a03a0f6fa19f25c11d7e4a54903e80
amuwaru <49254795+amuwaru@users.noreply.github.com>
2020-08-26 05:23:54 -0500

    Create SignatureMonoBehaviour.cs

---
+++ MonoBehaviours/SignatureMonoBehaviour.cs
@@ -0,0 +1,21 @@
+//made with amuwaru's unity c# templates
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+internal class #SCRIPTNAME# : MonoBehaviour
+{
+ //==FIELDS==\\ (public, internal, protected internal, protected, private protected, private)
+
+ //====================================================================================================\\
+
+ //==METHODS==\\ (public, internal, protected internal, protected, private protected, private)
+ public void Awake() {}
+ public void Start() {}
+ public void OnEnable() {}
+ public void OnDisable() {}
+ public void Update() {}
+ public void FixedUpdate() {}
+ public void LateUpdate() {}
+ //====================================================================================================\\
+}
